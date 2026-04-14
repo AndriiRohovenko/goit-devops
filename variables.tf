959193ec9cc735f5f92cdc0edbc6b8a5c0ff50d8
Andrii Rohovenko <andrii.rohovenko@Andriis-MacBook-Pro.local>
2026-04-14 22:11:53 +0300

    Update Helm chart for Django app, add postgres deployment and service

--- variables.tf
+++ variables.tf
@@ -75,19 +75,19 @@ variable "node_group_name" {
 variable "node_desired_size" {
   description = "Desired number of EKS worker nodes"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "node_min_size" {
   description = "Minimum number of EKS worker nodes"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "node_max_size" {
   description = "Maximum number of EKS worker nodes"
   type        = number
-  default     = 2
+  default     = 3
 }
 
 variable "node_instance_types" {
